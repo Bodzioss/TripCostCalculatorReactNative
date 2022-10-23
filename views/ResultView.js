d42eb81d0ede20b58360b842aeb68b508cd87bab
wiktord2000 <wiktord2000@wp.pl>
2022-10-23 20:24:35 +0200

    Initial ResultView

---
+++ views/ResultView.js
@@ -0,0 +1,72 @@
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
+function ResultView(){
+
+    return(
+        <View style={styles.container}>
+            <ScrollView>
+                <View style={styles.content}>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                    <Text>Result View</Text>
+                </View>
+            </ScrollView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    container: {
+        // Take all remaining space
+        flex: 1,
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
+
+export default ResultView;
