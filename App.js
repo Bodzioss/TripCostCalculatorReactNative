02aba0a6c0a73e7792b20317a99686382bd292a2
wiktord2000 <wiktord2000@wp.pl>
2022-10-23 20:24:47 +0200

    Update App component

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Button} from 'react-native';
+import TopBar from './shared/TopBar';
+import MainView from './views/MainView';
+import ResultView from './views/ResultView';
+import { POSSIBLE_VIEWS } from './constants/constants';
+
 
 export default function App() {
+
+  const [currentView, setCurrentView] = useState(POSSIBLE_VIEWS.MAIN);
+
+  function toggleView(){
+    setCurrentView((currentView) => currentView === POSSIBLE_VIEWS.MAIN ? POSSIBLE_VIEWS.RESULT : POSSIBLE_VIEWS.MAIN);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>  
+      {/* Status bar (battery status, wi-fi etc.) */}
+      <StatusBar backgroundColor='#003956' style='light'/>
+
+      {/* Main content */}
+      <View style={styles.container}>
+
+          {/* App name */}
+          <TopBar />
+
+          {/* Changing content */}
+          {currentView === POSSIBLE_VIEWS.MAIN ? <MainView /> : <ResultView />}
+
+          {/* Toggle view button*/}
+          <Button
+            title={currentView === POSSIBLE_VIEWS.MAIN ? 'OBLICZ' : 'POWRÓT'} 
+            color={currentView === POSSIBLE_VIEWS.MAIN && '#00c5a4'}
+            onPress={toggleView}
+          />
+
+      </View>
+    </>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
