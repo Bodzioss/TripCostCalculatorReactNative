50306362947461fa3557e66b810fdd7d4f079b9d
wiktord2000 <wiktord2000@wp.pl>
2022-10-29 00:20:14 +0200

    Update ResultView

--- views/ResultView/ResultView.js
+++ views/ResultView/ResultView.js
@@ -1,7 +1,27 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { COLORS } from '../../constants/AppConstants';
 import ResultCard from './components/ResultCard'
 
-function ResultView({passangers}){
+function ResultView({passengers}){
+
+    const getSumOfFuelCosts = (passengers) => {
+        return passengers
+                    .reduce((sum, passenger) => sum + passenger.fuelCost, 0)
+    }
+
+    const getSumOfPassengerAdditionalCosts = (passenger) => {
+        return passenger.additionalCosts
+                            .reduce((sum, additionalCost) => sum + additionalCost.price, 0);
+    }
+
+    const getSumOfAdditionalCosts = (passengers) => {
+        return passengers
+                    .reduce((sum, passenger) => sum + getSumOfPassengerAdditionalCosts(passenger), 0)
+    }
+
+    const sumOfFuelCosts = getSumOfFuelCosts(passengers);
+    const sumOfAdditionalCosts = getSumOfAdditionalCosts(passengers);
+    const allCosts = sumOfFuelCosts + sumOfAdditionalCosts;
 
     return(
         <View style={styles.container}>
@@ -9,18 +29,47 @@ function ResultView({passangers}){
                 <View style={styles.content}>
 
                     {/* Passangers list */}
-                    <View style={styles.passangersList}>
-                        {passangers.map(passanger => {
+                    <View style={styles.passengersList}>
+                        {passengers.map(passenger => {
                             return (
-                                <ResultCard key={passanger.id} passanger={passanger} />
+                                <ResultCard key={passenger.id} passenger={passenger} />
                             );
                         })}
                     </View>
+                    
+                    {/* Summary wrapper */}
+                    <View style={styles.summaryWrapper}>
+                        
+                        {/* Summary label */}
+                        <View style={styles.summaryLabel}>
+                            <Text style={styles.summaryText}>Podsumowanie</Text>
+                        </View>
 
-                    <View style={styles.divider}></View>
+                        {/* Summary content */}
+                        <View style={styles.summaryContent}>
+                            
+                            {/* Summary - fuel cost */}
+                            <View style={styles.label}>
+                                <Text style={styles.labelText}>Koszty paliwa</Text>
+                                <Text style={styles.labelPrice}>{sumOfFuelCosts.toFixed(2)} zł</Text>
+                            </View>
 
-                    <Text style={{textAlign: 'center', fontWeight: '500'}}>Podsumowanie</Text>
-                   
+                            {/* Summary - additional costs */}
+                            <View style={styles.label}>
+                                <Text style={styles.labelText}>Koszty dodatkowe</Text>
+                                <Text style={styles.labelPrice}>{sumOfAdditionalCosts.toFixed(2)} zł</Text>
+                            </View>
+
+                            {/* Summary - all */}
+                            <View style={[styles.label, styles.sumLabel]}>
+                                <Text style={styles.sumLabelText}>SUMA</Text>
+                                <Text style={styles.sumLabelPrice}>{allCosts.toFixed(2)} zł</Text>
+                            </View>
+
+                        </View>
+
+                    </View>
+
                 </View>
             </ScrollView>
         </View>
@@ -30,24 +79,72 @@ function ResultView({passangers}){
 const styles = StyleSheet.create({
 
     container: {
-        // Take all remaining space
-        flex: 1,
+        flex: 1,  // Take all remaining space
+        backgroundColor: '#00000055'
     },
     content: {
         flex: 1,
-        backgroundColor: 'red'
     },
-    passangersList: {
+    passengersList: {
         flex: 1,
-        marginVertical: 20,
-        marginHorizontal: 10,
-        backgroundColor: 'blue'
-    },
-    divider: {
-        height: 3,
-        backgroundColor: 'blue',
+        marginVertical: 10,
+        marginTop: 10,
         marginBottom: 20,
-        marginTop: 10
+    },
+    // Summary
+    summaryWrapper: {
+        backgroundColor: 'white',
+        paddingVertical: 16,
+    },
+    summaryLabel: {
+        padding: 16,
+        borderColor: COLORS.MAIN_BLUE,
+        borderBottomWidth: 4,
+        borderStyle: 'solid',
+        marginHorizontal: 28
+    },
+    summaryText: {
+        textAlign: 'center', 
+        fontWeight: '500', 
+        fontSize: 18,
+    },
+    summaryContent: {
+        paddingHorizontal: 30,
+        paddingVertical: 20
+    },
+    // Default label
+    label: {
+        paddingVertical: 10,
+        paddingHorizontal: 24,
+        marginVertical: 5,
+        borderRadius: 1000,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: COLORS.MAIN_GREY
+    },
+    labelText: {
+        opacity: 0.45,
+        fontWeight: '600'
+    },
+    labelPrice: {
+        fontWeight:'700', 
+        opacity: 0.8
+    },
+    // Sum label (use always with label class)
+    sumLabel: {
+        paddingVertical: 12,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: COLORS.MAIN_BLUE,
+        paddingRight: 26,
+    },
+    sumLabelText: {
+        color: 'white', 
+        fontWeight: '500'
+    },
+    sumLabelPrice: {
+        color: 'white', 
+        fontWeight:'700'
     }
 })
 
